9f544d82d3a1eacadf2bf63bd2064221553e3900
sedoid <karlsedoide@gmail.com>
2023-01-06 07:39:44 -0800

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,27 +5,10 @@ if __name__ == "__main__":
     import sys
 
     i = len(sys.argv) - 1
+    
+    arguments = sys.argv
 
+    print("{} arguments:".format(i))
 
-    if i == 0:
-
-        print("{} arguments.".format(i))
-
-    elif i == 1:
-
-        print("{} argument:".format(i))
-
-    else:
-
-        print("{} arguments:".format(i))
-
-
-
-    if i >= 1:
-
-        i = 0
-
-        for arg in sys.argv:
-                                                                                                          if i != 0:
-                                                                                                              print("{}: {}".format(i, arg))
-                                                                                                          i += 1
+    for i in range(1,len(arguments)):
+        print("{}:  {}").format(i,arguments[i])
