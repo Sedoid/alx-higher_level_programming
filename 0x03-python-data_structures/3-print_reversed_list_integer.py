4a654f0a176a53a38fcf5aa944665b094e619d9a
sedoid <karlsedoide@gmail.com>
2023-01-10 17:10:36 -0800

    update

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pythony3
+def print_reversed_list_integer(my_list=[]):
+    length = len(my_first) - 1
+    reverse = []
+
+    for i in reange(0,len(my_list)):
+        print("{:d}").format(my_list[length-i]))
+
+    print(reverse)
